03f3f09e995715d2c8eea3a7a866d33f9b977908
3ae <ogyshjsm@naver.com>
2023-09-20 09:59:35 +0900

    완성

--- design/lib/main.dart
+++ design/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MainApp()));
+  runApp(const MaterialApp(title: 'Flutter layout demo', home: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,13 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Column(
+    return Scaffold(
+        body: ListView(
       children: [
         Expanded(
-          child: Placeholder(),
+          child: Image.asset('assets/images/landscape.png', fit: BoxFit.cover),
         ),
-        Expanded(
+        const Expanded(
           flex: 2,
           child: Dialog(),
         )
@@ -29,31 +29,93 @@ class Dialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Title(),
-        Expanded(
-          child: Placeholder(),
-        )
-      ],
+    return Column(
+      children: [TitleSection(), ButtonSection(), Divider(), textSection],
     );
   }
 }
 
-class Title extends StatelessWidget {
-  const Title({super.key});
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(20),
+    final T = Theme.of(context).textTheme;
+
+    return Container(
+        padding: const EdgeInsets.all(32),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Oeschinen Lake Campground',
-                style: Theme.of(context).textTheme.titleMedium),
-            const Icon(Icons.star),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      'Oeschinen Lake Campground',
+                      style: T.titleMedium,
+                    ),
+                  ),
+                  Text(
+                    "Kandersteg, Switzeland",
+                    style: T.labelMedium,
+                  )
+                ],
+              ),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.orange,
+            ),
+            Text('41'),
           ],
         ));
   }
 }
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  Column _buildButton(Color color, IconData icon, String label) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Text(
+          label,
+          style: TextStyle(color: color),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        _buildButton(color, Icons.call, 'call'),
+        _buildButton(color, Icons.route, 'route'),
+        _buildButton(color, Icons.share, 'share'),
+      ]),
+    );
+  }
+}
+
+Widget textSection = const Padding(
+  padding: EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
